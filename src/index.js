dbbd56796b7c9eda7ade404135f18ff13c6da225
Pramith Prakash <pprakash2@sapient.com>
2021-05-12 23:29:50 +0530

    q

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+import $ from "jquery";
+
+
+/*
+let group = {
+  title: "Our Players",
+  players: ["John", "Pete", "Alice"],
+
+  showList() {
+    this.players.forEach(function (player) {
+      alert(this.title + ": " + player);
+    });
+  }
+};
+
+group.showList();
+*/
